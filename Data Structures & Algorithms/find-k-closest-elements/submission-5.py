c7db2633773678c870e7e4fd1460a0e2af10f733
Kevin <kevinxia2024@gmail.com>
2026-05-25 02:23:54 -0700

    Add: find-k-closest-elements - submission-5

---
+++ Data Structures & Algorithms/find-k-closest-elements/submission-5.py
@@ -0,0 +1,15 @@
+class Solution:
+    def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
+        # start at ends and enclose, don't add to new array
+
+        l, r = 0, len(arr) - 1
+
+        # iterate until window is equal to k
+
+        while r - l + 1 > k:
+            if abs(x - arr[l]) <= abs(x - arr[r]):
+                r -= 1
+            else:
+                l += 1
+
+        return arr[l:l+k]
